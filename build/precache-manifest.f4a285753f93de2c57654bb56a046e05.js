6154b7d0147b765f7d0e3cee67e6334cae25429b
alvaro-andrade <alvaro.andrade@salesforce.com>
2019-02-23 12:40:31 -0800

    Updated gitignore

---
+++ build/precache-manifest.f4a285753f93de2c57654bb56a046e05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d3407b42b6e9be53793e",
+    "url": "/static/js/main.d3407b42.chunk.js"
+  },
+  {
+    "revision": "b64bbb684e0d73e8da2a",
+    "url": "/static/js/2.b64bbb68.chunk.js"
+  },
+  {
+    "revision": "d3407b42b6e9be53793e",
+    "url": "/static/css/main.a3a640f6.chunk.css"
+  },
+  {
+    "revision": "b64bbb684e0d73e8da2a",
+    "url": "/static/css/2.b722efa0.chunk.css"
+  },
+  {
+    "revision": "03eae130a20c6f28e549d00f48b45c29",
+    "url": "/index.html"
+  }
+];
